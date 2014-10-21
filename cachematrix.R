a6da9261ac9428795c1be4309873626671ddeff3
Blazer86 <bill.mcknney@thrivent.com>
2014-10-20 22:34:01 -0500

    Test commit to master

--- cachematrix.R
+++ cachematrix.R
@@ -1,4 +1,4 @@
-## Put comments here that give an overall description of what your
+## Just testing what I need to do to commit changes to GIT
 ## functions do
 
 ## Write a short comment describing this function
